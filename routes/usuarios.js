b8bb56d1f21885f25cfc399784523d4c5ac7cd7b
luccameira <lucasmpronasa@gmail.com>
2025-05-21 13:56:42 -0300

    Update usuarios.js
    const db = require('../db');
    
    Correção > 
    
    const db = require('./db');

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../db');
+const db = require('./db');
 
 // Rota para cadastrar um novo usuário
 router.post('/', (req, res) => {
