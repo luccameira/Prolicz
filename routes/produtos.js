f892774942020f59dbc861a41cebb0c2c925dc65
luccameira <lucasmpronasa@gmail.com>
2025-05-21 18:09:54 -0300

    Update produtos.js

--- routes/produtos.js
+++ routes/produtos.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('./db').callback;
+const db = require('../db').callback;
 
 // GET /api/pedidos - Listagem com filtros e ordenações agrupada por pedido
 router.get('/', (req, res) => {
