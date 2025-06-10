d29d107835e100da18b7d7b80c872e9b264269b2
luccameira <lucasmpronasa@gmail.com>
2025-06-10 15:22:15 -0300

    Use bcryptjs and clean dependencies

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const connection = require('./db'); // ✅ conexão com callback
 
